45e93fe69bbe7f9550ee17ab12842a47cba1a5d3
romain sanson <romainsanson.gm@gmail.com>
2019-07-05 05:03:48 +0200

    api call & inject game cards

--- javascript/main.js
+++ javascript/main.js
@@ -1,27 +1,55 @@
 
 const myInit = {
-  headers: { 'X-Auth-Token': 'MY_API_KEY' },
+  headers: { 'X-Auth-Token': 'KEY' },
   dataType: 'json',
   type: 'GET',
 }
 
 const fetchGames = () => {
-  fetch('https://api.football-data.org/v2/teams/524/matches/', myInit)
-    .then((response) => {
-      return response.json()
-    })
-    .then((data) => {
-      console.log(data)
-    })
+  fetch('https://api.football-data.org/v2/competitions/FL1/matches', myInit)
+    .then(response => response.json())
+    .then(data => buildCards(data))
 }
 
-// const fetchTeam = (id) => {
-//   fetch(`https://api.football-data.org/v2/teams/${id}`, myInit)
-//     .then((response) => {
-//       return response.json()
-//     }).then((data) => {
-//       console.log(data)
-//     })
-// }
+const buildCards = (data) => {
+  const results = new Array
+  data.matches.forEach((match) => {
+    if (filter(match)) {
+      results.push(buildCard(match))
+    }
+  })
+  injectCards(results.slice(0, 3).join(''))
+}
 
-// fetchGames();
+const buildCard = (match) => {
+  console.log(match)
+  return `
+    <div class="col-sm">
+      <div class="game-card">
+        <div class="top-card">
+          <span class="home-team">${match.homeTeam.name}</span>
+          <span class="away-team">${match.awayTeam.name}</span>
+        </div>
+        <div class="bottom-card">
+          <span class="date">Thu, Jul 28 - 12:30 AM</span>
+          <span class="buy">Buy Tickets</span>
+          <span class="game-center">Game Center</span>
+        </div>
+      </div>
+    </div>
+  `
+}
+
+const injectCards = (html) => {
+  const row = document.querySelector('.upcoming-row')
+  row.innerHTML = html
+}
+
+
+const filter = (match) => {
+  if (match.awayTeam.name === 'Paris Saint-Germain FC' || match.homeTeam.name === 'Paris Saint-Germain FC') {
+    return true
+  }
+}
+
+fetchGames();
